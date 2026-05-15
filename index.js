ee78e94a6e69f190de57f2e04e7675a6af929fed
Dhairya <prabhakar.dhairya@gmail.com>
2026-05-15 09:21:01 +0530

    Testing Automated Jenkins Pipeline

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Webhook automation fully working')
+  res.send('Jenkins CI/CD Automation Successful')
 })
 app.listen(3000, () => {
   console.log('Server is running on port 3000')
